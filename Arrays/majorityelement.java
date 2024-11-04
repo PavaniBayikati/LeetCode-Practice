ff14ededd989c074a36ba73a8054a8f0f61dee7f
PavaniBayikati <bpavaniatp@gmail.com>
2024-11-04 16:52:14 +0530

    Programs

---
+++ Arrays/majorityelement.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class majorityelement {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n / 2];
+    }
+}
