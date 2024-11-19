00bcfc2b5ad0202cc172ddca2a4632578b8e93b9
Pavani Bayikati <161501559+PavaniBayikati@users.noreply.github.com>
2024-11-19 10:37:40 +0530

    Pr

---
+++ Neetcode150/Duplicates2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int length=nums.length;
+        for(int i=0;i<length;i++)
+        {
+            for(int j=i+1;j<length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
