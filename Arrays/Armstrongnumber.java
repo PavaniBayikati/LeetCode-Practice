38380347632de718711e4cdaf3f2c33a575676d8
Pavani Bayikati <161501559+PavaniBayikati@users.noreply.github.com>
2024-12-10 22:57:50 +0530

    Programs

--- Arrays/Armstrongnumber.java
+++ Arrays/Armstrongnumber.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Enter n value:-");
         int n=sc.nextInt();
         int original=n;
